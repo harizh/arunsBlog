2f51579866ac3052de870b57f86a24dd9d1f9a82
Pradeep PL <pradeeppl005@gmail.com>
2021-06-05 20:53:34 +0530

    Code Commit

---
+++ assets/js/functions.js
@@ -0,0 +1,20 @@
+$('.bannerCarousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    loop:true,
+    dots : true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
