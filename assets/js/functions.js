dc8b15b01383337219434139f9e959364ce34e75
Pradeep PL <pradeeppl005@gmail.com>
2021-06-05 22:27:34 +0530

    popular blogs

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -41,3 +41,24 @@ $('#blogsCarousel').owlCarousel({
         }
     }
 })
+
+$('#popularblogsCarousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    loop:true,
+    dots : true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
